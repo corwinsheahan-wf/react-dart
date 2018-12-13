31f515942eccc7752c6853c457b82445f270ddb4
Corwin Sheahan <corwin.sheahan@workiva.com>
2018-12-13 15:07:00 -0700

    expose eventPropKeyToEventFactory

--- lib/react_client.dart
+++ lib/react_client.dart
@@ -512,64 +512,65 @@ _convertEventHandlers(Map args) {
   });
 }
 
+var eventPropKeyToEventFactory = <String, Function>{
+  // SyntheticClipboardEvent
+  'onCopy': syntheticClipboardEventFactory,
+  'onCut': syntheticClipboardEventFactory,
+  'onPaste': syntheticClipboardEventFactory,
+
+  // SyntheticKeyboardEvent
+  'onKeyDown': syntheticKeyboardEventFactory,
+  'onKeyPress': syntheticKeyboardEventFactory,
+  'onKeyUp': syntheticKeyboardEventFactory,
+
+  // SyntheticFocusEvent
+  'onFocus': syntheticFocusEventFactory,
+  'onBlur': syntheticFocusEventFactory,
+
+  // SyntheticFormEvent
+  'onChange': syntheticFormEventFactory,
+  'onInput': syntheticFormEventFactory,
+  'onSubmit': syntheticFormEventFactory,
+  'onReset': syntheticFormEventFactory,
+
+  // SyntheticMouseEvent
+  'onClick': syntheticMouseEventFactory,
+  'onContextMenu': syntheticMouseEventFactory,
+  'onDoubleClick': syntheticMouseEventFactory,
+  'onDrag': syntheticMouseEventFactory,
+  'onDragEnd': syntheticMouseEventFactory,
+  'onDragEnter': syntheticMouseEventFactory,
+  'onDragExit': syntheticMouseEventFactory,
+  'onDragLeave': syntheticMouseEventFactory,
+  'onDragOver': syntheticMouseEventFactory,
+  'onDragStart': syntheticMouseEventFactory,
+  'onDrop': syntheticMouseEventFactory,
+  'onMouseDown': syntheticMouseEventFactory,
+  'onMouseEnter': syntheticMouseEventFactory,
+  'onMouseLeave': syntheticMouseEventFactory,
+  'onMouseMove': syntheticMouseEventFactory,
+  'onMouseOut': syntheticMouseEventFactory,
+  'onMouseOver': syntheticMouseEventFactory,
+  'onMouseUp': syntheticMouseEventFactory,
+
+  // SyntheticTouchEvent
+  'onTouchCancel': syntheticTouchEventFactory,
+  'onTouchEnd': syntheticTouchEventFactory,
+  'onTouchMove': syntheticTouchEventFactory,
+  'onTouchStart': syntheticTouchEventFactory,
+
+  // SyntheticUIEvent
+  'onScroll': syntheticUIEventFactory,
+
+  // SyntheticWheelEvent
+  'onWheel': syntheticWheelEventFactory,
+};
+
+
 /// A mapping from event prop keys to their respective event factories.
 ///
 /// Used in [_convertEventHandlers] for efficient event handler conversion.
 final Map<String, Function> _eventPropKeyToEventFactory = (() {
-  var eventPropKeyToEventFactory = <String, Function>{
-    // SyntheticClipboardEvent
-    'onCopy': syntheticClipboardEventFactory,
-    'onCut': syntheticClipboardEventFactory,
-    'onPaste': syntheticClipboardEventFactory,
-
-    // SyntheticKeyboardEvent
-    'onKeyDown': syntheticKeyboardEventFactory,
-    'onKeyPress': syntheticKeyboardEventFactory,
-    'onKeyUp': syntheticKeyboardEventFactory,
-
-    // SyntheticFocusEvent
-    'onFocus': syntheticFocusEventFactory,
-    'onBlur': syntheticFocusEventFactory,
-
-    // SyntheticFormEvent
-    'onChange': syntheticFormEventFactory,
-    'onInput': syntheticFormEventFactory,
-    'onSubmit': syntheticFormEventFactory,
-    'onReset': syntheticFormEventFactory,
-
-    // SyntheticMouseEvent
-    'onClick': syntheticMouseEventFactory,
-    'onContextMenu': syntheticMouseEventFactory,
-    'onDoubleClick': syntheticMouseEventFactory,
-    'onDrag': syntheticMouseEventFactory,
-    'onDragEnd': syntheticMouseEventFactory,
-    'onDragEnter': syntheticMouseEventFactory,
-    'onDragExit': syntheticMouseEventFactory,
-    'onDragLeave': syntheticMouseEventFactory,
-    'onDragOver': syntheticMouseEventFactory,
-    'onDragStart': syntheticMouseEventFactory,
-    'onDrop': syntheticMouseEventFactory,
-    'onMouseDown': syntheticMouseEventFactory,
-    'onMouseEnter': syntheticMouseEventFactory,
-    'onMouseLeave': syntheticMouseEventFactory,
-    'onMouseMove': syntheticMouseEventFactory,
-    'onMouseOut': syntheticMouseEventFactory,
-    'onMouseOver': syntheticMouseEventFactory,
-    'onMouseUp': syntheticMouseEventFactory,
-
-    // SyntheticTouchEvent
-    'onTouchCancel': syntheticTouchEventFactory,
-    'onTouchEnd': syntheticTouchEventFactory,
-    'onTouchMove': syntheticTouchEventFactory,
-    'onTouchStart': syntheticTouchEventFactory,
-
-    // SyntheticUIEvent
-    'onScroll': syntheticUIEventFactory,
-
-    // SyntheticWheelEvent
-    'onWheel': syntheticWheelEventFactory,
-  };
-
   // Add support for capturing variants; e.g., onClick/onClickCapture
   for (var key in eventPropKeyToEventFactory.keys.toList()) {
     eventPropKeyToEventFactory[key + 'Capture'] = eventPropKeyToEventFactory[key];
